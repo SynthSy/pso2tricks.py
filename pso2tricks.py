ba3676a6ef3a8326085f9a17ac316d9b22041a37
SynthSy <synthsy@gmail.com>
2026-05-21 23:00:12 -0700

    Clean up code to not use ``os.system`` + Version bump

--- pso2tricks.py
+++ pso2tricks.py
@@ -10,6 +10,10 @@ import argparse
 import getpass
 import re
 import requests
+import shutil
+import stat
+import zipfile
+import subprocess
 from pathlib import Path
 
 # I use tabs instead of spaces like a monster :)
@@ -19,7 +23,6 @@ class prettyHelp(argparse.ArgumentDefaultsHelpFormatter):
 		if action.option_strings:
 			return ', '.join(action.option_strings)
 		return super()._format_action_invocation(action)
-
 class PSO2TweakerPy:
 	def __init__(self):
 		self.parser = argparse.ArgumentParser(
@@ -42,7 +45,7 @@ class PSO2TweakerPy:
 
 	def _version(self, info):
 		if info is True:
-			version = '1.5'
+			version = '1.6'
 			print('pso2tricks v' + version)
 
 	def regexPath(self, directory, pattern): # check if file exists by matching regex
@@ -77,13 +80,14 @@ class PSO2TweakerPy:
 			if os.path.exists(f'{self.tweaker_folder}') or os.getcwd() == self.tweaker_folder: 
 				pass
 			elif os.getcwd() == self.homeDir:
-				os.system(f'mkdir {self.tweaker_folder}')
+				#os.system(f'mkdir {self.tweaker_folder}')
+				Path(self.tweaker_folder).mkdir(parents=False, exist_ok=False)
 			else:
 				pass
 
 			if (update is True and os.path.exists(f'./{tweakerExe}')) or os.path.exists(f'./{tweakerExe}'):
 				print('Updating the PSO2 Tweaker from https://arks-layer.com...')
-				os.system(f'cd {self.tweaker_folder} && rm "PSO2 Tweaker.exe"')
+				call(f'cd {self.tweaker_folder} && rm "PSO2 Tweaker.exe"', shell=True)
 				path = f'{self.tweaker_folder}/{tweakerExe}'
 				os.remove(path)
 
@@ -94,28 +98,40 @@ class PSO2TweakerPy:
 			self.downloadFile(tweakerUrl, filepath)
 
 	def patchDownload(self, patch, pso2_bin): # download patches if the tweaker did not
-		patcher = 'https://synthsy.space/els_linux' # https://github.com/HybridEidolon/pso2-modpatcher || I compiled a binary using ubuntu 20.04 then uploaded it.
+		patcher = 'https://synthsy.space/json/els_linux' # https://github.com/HybridEidolon/pso2-modpatcher || I compiled a binary using ubuntu 20.04 then uploaded it.
 		ngs_en_patch = 'https://cdn.arks-layer.com/TweakerTemp/Latest_Patch_EN_Reboot.zip'
 		classic_en_patch = 'https://cdn.arks-layer.com/TweakerTemp/Latest_Patch_EN_win32.zip'
+		els_path = f'{self.tweaker_folder}/ELS'
 
 		print('Deleting old patch files before downloading new ones...')
 		if os.path.exists(f'{self.tweaker_folder}/ELS'):
-			path = f'{self.tweaker_folder}/ELS'
-			os.system(f'rm -rf {path}')
-			os.system(f'cd {self.tweaker_folder} && mkdir ELS')
+			shutil.rmtree(els_path)
+			Path(els_path).mkdir(parents=False, exist_ok=False)
 		else:
-			os.system(f'cd {self.tweaker_folder} && mkdir ELS')
+			Path(els_path).mkdir(parents=False, exist_ok=False)
 
 		print('Downloading required files...')
 		elsFilename = f'{self.tweaker_folder}/els_linux'
 		self.downloadFile(patcher, elsFilename)
-		os.system(f'cd {self.tweaker_folder} && chmod +x ./els_linux') # required or we can't execute the command
+		os.chmod(elsFilename, 0o755) # required or we can't execute the command
 		if str(patch) == 'ngs':
 			reboot_zip = f'{self.tweaker_folder}/Latest_Patch_EN_Reboot.zip'
 			self.downloadFile(ngs_en_patch, reboot_zip)
-			os.system(f'cd {self.tweaker_folder} && unzip Latest_Patch_EN_Reboot.zip -d "{self.tweaker_folder}/ELS"')
-			os.system(f'cd {self.tweaker_folder} && ./els_linux --no-backup -v "{self.tweaker_folder}/ELS/win32" "{pso2_bin}/data/win32"')
-			os.system(f'cd {self.tweaker_folder} && ./els_linux --no-backup -v "{self.tweaker_folder}/ELS/win32reboot" "{pso2_bin}/data/win32reboot"')
+			
+			with zipfile.ZipFile(reboot_zip, "r") as zip_ref:
+				zip_ref.extractall(f'{self.tweaker_folder}/ELS')
+			win32_dir = f'{self.tweaker_folder}/ELS/win32'
+			win32reboot_dir = f'{self.tweaker_folder}/ELS/win32reboot'
+			
+			result = subprocess.run(['./els_linux', '--no-backup', '-v', win32_dir, f'{pso2_bin}/data/win32'],
+				cwd=self.tweaker_folder,
+				check=True
+			)
+			reboot = subprocess.run(['./els_linux', '--no-backup', '-v', win32reboot_dir, f'{pso2_bin}/data/win32reboot'],
+				cwd=self.tweaker_folder,
+				check=True
+			)
+			
 			print('Cleaning up...')
 			os.remove(reboot_zip)
 			print('done')
@@ -125,10 +141,18 @@ class PSO2TweakerPy:
 			win32_zip = f'{self.tweaker_folder}/Latest_Patch_EN_win32.zip'
 			self.downloadFile(classic_en_patch, win32_zip)
 			self.downloadFile(ngs_en_patch, reboot_zip)
-			os.system(f'cd {self.tweaker_folder} && unzip Latest_Patch_EN_win32.zip -d "{self.tweaker_folder}/ELS"')
-			os.system(f'cd {self.tweaker_folder} && unzip Latest_Patch_EN_Reboot.zip -d "{self.tweaker_folder}/ELS"')
-			os.system(f'cd {self.tweaker_folder} && ./els_linux -v --no-backup "{self.tweaker_folder}/ELS/win32" "{pso2_bin}/data/win32"')
-			os.system(f'cd {self.tweaker_folder} && ./els_linux -v --no-backup "{self.tweaker_folder}/ELS/win32reboot" "{pso2_bin}/data/win32reboot"')
+			with zipfile.ZipFile(win32_zip, "r") as win32:
+				win32.extractall(f'{self.tweaker_folder}/ELS')
+			with zipfile.ZipFile(reboot_zip, "r") as reboot:
+				reboot.extractall(f'{self.tweaker_folder}/ELS')
+			result = subprocess.run(['./els_linux', '--no-backup', '-v', win32_dir, f'{pso2_bin}/data/win32'],
+				cwd=self.tweaker_folder,
+				check=True
+			)
+			reboot = subprocess.run(['./els_linux', '--no-backup', '-v', win32reboot_dir, f'{pso2_bin}/data/win32reboot'],
+				cwd=self.tweaker_folder,
+				check=True
+			)
 			print('Cleaning up...')
 			os.remove(reboot_zip)
 			os.remove(win32_zip)
@@ -149,4 +173,3 @@ class PSO2TweakerPy:
 if __name__ == "__main__":
 	app = PSO2TweakerPy()
 	app.parseArgs()
-main
